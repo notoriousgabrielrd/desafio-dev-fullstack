1dfb06617f57a5c373aad4a8c3287c549402b844
gabrielDellrado <grddourado@cosmobots.io>
2024-10-28 00:46:13 -0300

    leads service && global pipes

---
+++ src/leads/leads.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma/prisma.service';
+import { CreateLeadDto } from './dto/create-lead.dto';
+import { last12Months } from 'src/utils/utils';
+
+@Injectable()
+export class LeadsService {
+    constructor(private prisma: PrismaService) { }
+
+    async createLead({ nomeCompleto, email, telefone, unidades }: CreateLeadDto) {
+        const response = this.prisma.lead.create({
+            data: {
+                nomeCompleto,
+                email,
+                telefone,
+                unidades: {
+                    create: unidades.map(({ codigoDaUnidadeConsumidora, modeloFasico, enquadramento, historicoDeConsumoEmKWH }) => ({
+                        codigoDaUnidadeConsumidora,
+                        modeloFasico,
+                        enquadramento,
+                        historicoDeConsumoEmKWH: {
+                            create: historicoDeConsumoEmKWH.map(({ consumoForaPontaEmKWH, mesDoConsumo }) => ({
+                                consumoForaPontaEmKWH,
+                                mesDoConsumo,
+                            })),
+                        },
+                    })),
+                },
+            },
+        });
+
+        return response
+    }
+
+    async getLeads(filters: { nomeCompleto?: string; email?: string; codigoDaUnidadeConsumidora?: string }) {
+
+        const { nomeCompleto, email, codigoDaUnidadeConsumidora } = filters;
+
+        const response = this.prisma.lead.findMany({
+            where: {
+                nomeCompleto: nomeCompleto ? { contains: nomeCompleto } : undefined,
+                email: email ? { contains: email } : undefined,
+                unidades: codigoDaUnidadeConsumidora
+                    ? { some: { codigoDaUnidadeConsumidora: { contains: codigoDaUnidadeConsumidora } } }
+                    : undefined,
+            },
+            include: {
+                unidades: {
+                    include: {
+                        historicoDeConsumoEmKWH: {
+                            where: {
+                                mesDoConsumo: {
+                                    gte: last12Months()
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+        });
+
+        return response
+    }
+
+    async getLeadById(id: string) {
+        const response = this.prisma.lead.findUnique({
+            where: { id },
+            include: {
+                unidades: {
+                    include: {
+                        historicoDeConsumoEmKWH: {
+                            where: {
+                                mesDoConsumo: {
+                                    gte: last12Months()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        return response
+    }
+}
