5f92591a0c715d94d5036604bd65088fb3c51e2b
gabrielDellrado <grddourado@cosmobots.io>
2024-10-28 15:53:16 -0300

    configurando CORS

--- new-sun-energy/src/main.ts
+++ new-sun-energy/src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe)
-  await app.listen(process.env.PORT ?? 3000);
+   app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
