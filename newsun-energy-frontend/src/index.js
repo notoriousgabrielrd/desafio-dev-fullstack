0fa16a72d36ae656ff86985eff0a759466724c7a
gabrielDellrado <grddourado@cosmobots.io>
2024-10-28 17:34:30 -0300

    configurando toastify

--- newsun-energy-frontend/src/index.js
+++ newsun-energy-frontend/src/index.js
@@ -6,11 +6,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+    <ToastContainer />
       <App />
     </ChakraProvider>
   </React.StrictMode>
